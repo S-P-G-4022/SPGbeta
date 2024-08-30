66d196ee0b91028db2cbf90d2ee4f8557d52e337
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-08-30 22:12:46 +0100

    GET Reed of unused code and  sign in suceccfully

--- SPG-A/src/auth/AuthWrapper.js
+++ SPG-A/src/auth/AuthWrapper.js
@@ -1,45 +1,40 @@
-import { createContext, useContext, useState } from "react"
-import { RenderMenu, RenderRoutes } from "../components/structure/RenderNavigation";
+import { createContext, useContext, useState } from "react";
+import {
+  RenderMenu,
+  RenderRoutes,
+} from "../components/structure/RenderNavigation";
+import axios from "axios";
 
 const AuthContext = createContext();
 export const AuthData = () => useContext(AuthContext);
 
-
 export const AuthWrapper = () => {
+  const [user, setUser] = useState({ name: "", isAuthenticated: false });
 
-     const [ user, setUser ] = useState({name: "", isAuthenticated: false})
+  const login = async (email, password) => {
+    try {
+      let res = await axios.post("http://localhost:3000/api/user/signin", {
+        email: email,
+        password: password,
+      });
+      if (res.data.message === "Login succeeded") {
+        setUser({ name: res.data.loggedUser.FullName, isAuthenticated: true });
+      } 
+      return res.data;
+    } catch (e) {
+      return e;
+    }
+  };
+  const logout = () => {
+    setUser({ ...user, isAuthenticated: false });
+  };
 
-     const login = (userName, password) => {
-
-          
-          return new Promise((resolve, reject) => {
-
-               if (password === "password") {
-                    setUser({name: userName, isAuthenticated: true})
-                    resolve("success")
-               } else {
-                    reject("Incorrect password")
-               }
-          })
-          
-          
-     }
-     const logout = () => {
-
-          setUser({...user, isAuthenticated: false})
-     }
-
-
-     return (
-          
-               <AuthContext.Provider value={{user, login, logout}}>
-                    <>
-                         <RenderMenu />
-                         <RenderRoutes />
-                    </>
-                    
-               </AuthContext.Provider>
-          
-     )
-
-}
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      <>
+        <RenderMenu />
+        <RenderRoutes />
+      </>
+    </AuthContext.Provider>
+  );
+};
