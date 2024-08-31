df652413150163b69d8fe4ac47f39325866b083e
aminelaarif <laarifamine5569@gmail.com>
2024-08-31 20:06:08 +0100

    get one game controller

--- server/controller/games.js
+++ server/controller/games.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+const prisma = require("../db/prisma.js")
 
 
 // Fetch the list of all games
@@ -17,9 +17,50 @@ async function fetchGameDetails(appid) {
 
 
 
+
+const oneGame = async (req,res)=>{
+    
+    try{
+        const {id} = req.params
+
+        const game = await prisma.Game.findUnique({
+            where :{
+                id : parseInt(id)
+            }
+        })
+       res.status(200).send(game)
+    }catch(error){
+        console.log(error)
+        res.status(400).send(error)
+    }
+}
+
+
+const fetchGames = async (req,res) => {
+    console.log(req.body,"ahahahahahahahahahah")
+    const {names} =req.body
+    try {
+       
+  const games =  await prisma.Game.findMany({
+    where: {
+        names :{
+            contains : names
+        }
+        
+      },
+  })
+  res.status(200).send(games)
+    }catch(error){
+        console.log(error)
+        res.status(401).send(error)
+    }
+}
+
 module.exports={
 
 fetchGameDetails,
 fetchAllGames,
+fetchGames,
+oneGame
 }
 
