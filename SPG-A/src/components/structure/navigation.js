3b05d7555757ae6f50caa6e670f22b0f4b8519fd
aminelaarif <laarifamine5569@gmail.com>
2024-08-31 20:13:41 +0100

    new paths for game profile and searched components

--- SPG-A/src/components/structure/navigation.js
+++ SPG-A/src/components/structure/navigation.js
@@ -4,6 +4,8 @@ import { Home } from "../pages/Home"
 import  Login  from "../pages/Login"
 import { Private } from "../pages/Private"
 import Logup  from "../pages/Logup"
+import Searchedcomponent from "../pages/home/Searchedcomponent"
+import OneGame from "../pages/home/OneGame"
 
 export const nav = [
      { path:     "/",         name: "Home",        element: <Home />,       isMenu: true,     isPrivate: false  },
@@ -12,4 +14,7 @@ export const nav = [
      { path:     "/private",  name: "Private",     element: <Private />,    isMenu: true,     isPrivate: true  },
      { path:     "/account",  name: "Account",     element: <Account />,    isMenu: true,     isPrivate: true  },
      { path:     "/signUp",  name: "signUp",      element: <Logup />,       isMenu: true,     isPrivate: false  },
+     { path:     "/searched",  name: "Searched",      element: <Searchedcomponent/>,       isMenu: false,     isPrivate: false  },
+     { path:     "/onegame",  name: "onegame",      element: <OneGame/>,       isMenu: false,     isPrivate: false  },
+     
 ]
